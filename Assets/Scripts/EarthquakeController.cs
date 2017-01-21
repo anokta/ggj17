c7fe24094d181ef65c11c74479116b9c0e549ef0
Robert Soares <argher@gmail.com>
2017-01-20 23:31:25 -0800

    added fracturing

--- Assets/Scripts/EarthquakeController.cs
+++ Assets/Scripts/EarthquakeController.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EarthquakeController : MonoBehaviour {
-  // Upwards modifier to lift objects via explosion.
-  public float upwardsModifier = 2.0f;
+    // Upwards modifier to lift objects via explosion.
+    public GameObject cubeFragments;
+    public float upwardsModifier = 2.0f;
 
   // Sends an explosive force within |radius| from the origin |position|.
-  public void AddForce (Vector3 position, float power, float radius, ForceMode mode) {
-    Collider[] colliders = Physics.OverlapSphere(position, radius);
-    foreach (var collider in colliders) {
-      Rigidbody body = collider.GetComponent<Rigidbody>();
-      if (body != null) {
-        body.AddExplosionForce(power, position, radius, upwardsModifier, mode);
-      }
-    }
+    public void AddForce (Vector3 position, float power, float radius, ForceMode mode) {
+        Collider[] colliders = Physics.OverlapSphere(position, radius);
+        foreach (var collider in colliders)
+        {
+            Debug.Log(collider.tag);
+            if (collider.tag != "Ground" && collider.tag != "Fragment")
+            {
+                Instantiate(cubeFragments, collider.transform.position, collider.transform.rotation);
+                Rigidbody[] fragmentBodies = cubeFragments.GetComponentsInChildren<Rigidbody>();
+                Destroy(collider.gameObject);
+                if (fragmentBodies != null)
+                {
+                    foreach (var fragmentBody in fragmentBodies)
+                    {
+                        fragmentBody.AddExplosionForce(power, position, radius, upwardsModifier, mode);
+                    }
+                }
+            }
+        }
   }
 }
---
+++ Assets/Scripts/FractureCleanup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureCleanup : MonoBehaviour {
+    public float fragmentDuration;
+    // Use this for initialization
+    void Start()
+    {
+        tagChildren();
+        StartCoroutine(Cleanup());
+    }
+
+    IEnumerator Cleanup()
+    {
+        yield return new WaitForSeconds(fragmentDuration);
+        Destroy(gameObject);
+    }
+    void tagChildren()
+    {
+        Transform[] childTransforms = GetComponentsInChildren<Transform>();
+        foreach (Transform t in childTransforms)
+        {
+            t.gameObject.tag = "Fragment";
+        }
+    }
+
+}
