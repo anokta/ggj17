70a4d566144b45124aab18dbd317acea30fcf192
Alper Gungormusler <anokta@gmail.com>
2017-01-20 20:34:07 -0800

    initial attempt to create explosion waves.

---
+++ Assets/Scripts/EarthquakeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthquakeController : MonoBehaviour {
+  // Upwards modifier to lift objects via explosion.
+  public float upwardsModifier = 2.0f;
+
+  // Sends an explosive force within |radius| from the origin |position|.
+  public void AddForce (Vector3 position, float power, float radius, ForceMode mode) {
+    Collider[] colliders = Physics.OverlapSphere(position, radius);
+    foreach (var collider in colliders) {
+      Rigidbody body = collider.GetComponent<Rigidbody>();
+      if (body != null) {
+        body.AddExplosionForce(power, position, radius, upwardsModifier, mode);
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/Test/ExplosionTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTest : MonoBehaviour {
+
+  public EarthquakeController earthquake;
+
+  public LayerMask groundMask;
+
+  // Force power in kg*m/s^2.
+  public float forcePower = 10.0f;
+ 
+  // Force radius in meters.
+  public float forceRadius = 10.0f;
+
+  // Impulse power in kg*m/s^2.
+  public float impulsePower = 5.0f;
+
+  // Impulse radius in meters.
+  public float impulseRadius = 2.5f;
+
+  private readonly float maxRaycastDistance = 100.0f;
+
+  public float sineAmplitude = 1.0f;
+  public float sineFrequency = 0.5f;
+  private float phasor = 0.0f;
+
+  void Update () {
+    RaycastHit hit;
+    bool success = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 
+                                   maxRaycastDistance, groundMask.value);
+    if (!success) {
+      return;
+    }
+
+    Vector3 scale = Vector3.zero;
+    Vector3 position = hit.point;
+    if (Input.GetMouseButtonDown(0)) {
+      transform.position = position;
+      // Send an impulse on click.
+      earthquake.AddForce(position, impulsePower, impulseRadius, ForceMode.Impulse);
+      scale = Vector3.one * impulseRadius;
+    } else if (Input.GetMouseButton(1)) {
+      scale = Vector3.one * forceRadius;
+      position.y = sineAmplitude * Mathf.Sin(2.0f * Mathf.PI * phasor);
+      phasor += sineFrequency * Time.deltaTime;
+      if (phasor >= 1.0f) {
+        phasor -= 1.0f; 
+      }
+
+      transform.position = position;
+      earthquake.AddForce(position, forcePower, forceRadius, ForceMode.Force);
+
+    }
+
+    transform.localScale = 0.5f * scale;
+  }
+}
