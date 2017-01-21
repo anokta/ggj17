0f85da58c4148264ade0cc0acbde25a435c5f982
Daniel C <danclayson@gmail.com>
2017-01-21 12:26:04 -0800

    Roads now populate in city

--- Assets/Scripts/cityBuilder.cs
+++ Assets/Scripts/cityBuilder.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CityBuilder : MonoBehaviour {
   // Building prefab.
   public GameObject buildingPrefab;
+	public GameObject roadStraightPrefab;
+	public GameObject road4wayPrefab;
  
   // Number of buildings per axis.
   public int xCount = 10;
@@ -24,14 +26,42 @@ public class CityBuilder : MonoBehaviour {
     cityRoot = new GameObject("City");
 
     for (int x = 0; x < xCount; ++x) {
-      for (int y = 0; y < yCount; ++y) {
-        float height = Random.Range(minHeight, maxHeight);
-        Vector3 position = new Vector3(2.0f * x - xCount, 0.5f * height, 2.0f * y - yCount);
+			for (int y = 0; y < yCount; ++y) {
+				Vector3 position = new Vector3(x - xCount, .05f, y - yCount);
 
-        GameObject building = 
-          (GameObject) GameObject.Instantiate(buildingPrefab, position, Quaternion.identity, 
-                                              cityRoot.transform);
-        building.transform.localScale = new Vector3(1.0f, height, 1.0f);
+				//Every fourth tile is a road
+				if (x % 4 == 0) {
+					//Every third tile is a cross street, so make it a 4 way;
+					if (y % 3 == 0) {
+						GameObject roadTile = 
+							(GameObject) GameObject.Instantiate(road4wayPrefab, position, Quaternion.identity, 
+								cityRoot.transform);
+
+					} else {
+						//Since it's not a cross street, make it a standard street;
+						GameObject roadTile = 
+							(GameObject) GameObject.Instantiate(roadStraightPrefab, position, Quaternion.identity, 
+								cityRoot.transform);
+						roadTile.transform.localEulerAngles = new Vector3 (0f, 90f, 0f);
+					}
+				} else {
+					if (y % 3 == 0) {
+						//In this case, it's a street
+						GameObject roadTile = 
+							(GameObject) GameObject.Instantiate(roadStraightPrefab, position, Quaternion.identity, 
+								cityRoot.transform);
+					} else {
+						//Not a street, and not a building. Let's build!
+						float height = Random.Range(minHeight, maxHeight);
+						position.y = height / 2f;
+						GameObject building = 
+							(GameObject) GameObject.Instantiate(buildingPrefab, position, Quaternion.identity, 
+								cityRoot.transform);
+						building.transform.localScale = new Vector3(.9f, height, .9f);
+					}
+				}
+
+
       }
     }
   }
