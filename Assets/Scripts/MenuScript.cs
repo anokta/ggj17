2165155216bcbcdaa8c1f8b63895e8ed7e948939
Daniel C <danclayson@gmail.com>
2017-01-21 15:58:33 -0800

    Added Start Scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+
+	public void ExitPress()
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress()
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
